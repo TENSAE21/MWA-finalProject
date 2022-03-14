c6d3c2c13e5b788e9a680b7a12460a909b2bb575
Tensae <tensaeberhan@ymail.com>
2022-03-14 17:05:40 -0500

    merged

--- ExamPrep-BackEnd/app.js
+++ ExamPrep-BackEnd/app.js
@@ -1,16 +1,29 @@
 const express = require('express')
+const morgan = require('morgan')
+const fs = require('fs')
 const cors = require('cors')
+const qaRouter = require('./routes/qaRoute')
 
 const app = express()
 
 app.use(cors())
+app.use(express.json())
+app.use(morgan('common', {
+    stream: fs.createWriteStream('./access.log', {flags: 'a'})
+}));
+app.use(morgan('dev'));
+app.use("public", express.static("assets/pics"))
+
+app.use("/qa", qaRouter)
+
+app.get("/", (req,res)=>{
+    res.send({result: "ok"})
+})
 
 
-
-
-
-
-
+app.use((err,req,res,next)=>{
+    return res.json({error: err.message})
+})
 
 
 app.listen(3000, _=>console.log(`running on port 3000`))
--- ExamPrep-FrontEnd/examprep/src/app/app.module.ts
+++ ExamPrep-FrontEnd/examprep/src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+
 import { QuestionsComponent } from './questions/questions.component';
+
+import { LoginComponent } from './login/login.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuestionsComponent
+    QuestionsComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      // { path: '', component: HomeComponent },
+      { path: 'login', component: LoginComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
