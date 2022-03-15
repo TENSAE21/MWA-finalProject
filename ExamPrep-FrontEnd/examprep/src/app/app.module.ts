56bb5bd4f6a167d14d725e6fcbc99e3c58bd3867
BiniamGirmay <bgirmay@miu.edu>
2022-03-14 19:03:08 -0500

    ch

--- ExamPrep-FrontEnd/examprep/src/app/app.module.ts
+++ ExamPrep-FrontEnd/examprep/src/app/app.module.ts
@@ -1,14 +1,27 @@
-import { NgModule } from '@angular/core';
+// built-in
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+// components
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { SignUpComponent } from './user/sign-up/sign-up.component';
+//routes
+import { appRoutes } from './routes';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    SignUpComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
