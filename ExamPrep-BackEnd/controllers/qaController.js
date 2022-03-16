a8a388164b896f34856c13458584809237efb429
Tensae <tensaeberhan@ymail.com>
2022-03-16 04:02:37 -0500

    harmonized backend, not included: config folder

--- ExamPrep-BackEnd/controllers/qaController.js
+++ ExamPrep-BackEnd/controllers/qaController.js
@@ -2,34 +2,39 @@ const dao = require("../dao/qaDao");
 
 
 // get all
-function getAll(req, res) {
-  const ret = dao.getAll()
-  // console.log(typeOf(ret))
-  return res.send({data: ret});
+async function getAll(req, res) {
+  const ret = await dao.getAll()
+  console.log(ret)
+  return res.json(ret);
 }
 
 // add new question
-function addQuestion(req, res){
+async function addQuestion(req, res){
   const questionBody = req.body;
-  return res.json(dao.addOne(questionBody));
+  const returned = dao.addOne(questionBody);
+  return res.json(returned);
 }
 
 // get one by id
-function getById(req, res) {
-  const id = parseInt(req.params.id);
-  return res.json(dao.getById(id));
+async function getById(req, res) {
+  const returned = await dao.getById(req.params.id)
+  return res.json(returned);
 }
 
 // delete one
-function deleteOne(req, res) {
-  const id = parseInt(req.params.id);
-  dao.removeOne(id);
+async function deleteOne(req, res) {
+  const deleted = await dao.removeOne(req.params.id);
+  console.log(deleted)
   return res.status(200).json({ result: "ok" });
 }
+
+
+// {question:'Which of the following is used to share data between controller and view in Angular?',CA:' A)using Model',CB:'B) using services',
+//CC:'C) using factory',CD:' D) using $scope',Ans:'A'}
 
 module.exports = {
   getAll,
   getById,
   deleteOne,
-  addQuestion 
+  addQuestion
 };
