2b1a220783e8ff9103a641c01b12613d7b17e2d6
Tensae Berhan <TENSAE21@users.noreply.github.com>
2022-03-18 11:05:49 -0500

    Update index.router.js

--- ExamPrep-BackEnd/routes/index.router.js
+++ ExamPrep-BackEnd/routes/index.router.js
@@ -8,5 +8,6 @@ const jwtHelper = require('../config/jwtHelper');
 router.post('/register', ctrlUser.register);
 router.post('/authenticate', ctrlUser.authenticate);
 router.get('/userProfile',jwtHelper.verifyJwtToken, ctrlUser.userProfile);
+router.get('/', ctrlUser.getAllUsers)
 
 module.exports = router;
